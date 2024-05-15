bb384cbafaa330f4e93959c06b6288223c85cacc
yiyilopez <146986398+yiyilopez@users.noreply.github.com>
2024-05-14 21:55:29 -0500

    Fix issues regarding the methods implemented

--- src/main/java/culturemedia/service/CulturemediaService.java
+++ src/main/java/culturemedia/service/CulturemediaService.java
@@ -11,6 +11,8 @@ public interface CulturemediaService {
 
     List<Video> findAll() throws VideoNotFoundException;
     Video add(Video video);
-    View addView(View view);   
-
+    View addView(View view); 
+    List<Video> findByTitle(String title) throws VideoNotFoundException;
+    List<Video> findByDuration(Double fromDuration, Double toDuration) throws VideoNotFoundException;
+    
 }
--- src/test/java/culturemedia/service/CulturemediaServiceTest.java
+++ src/test/java/culturemedia/service/CulturemediaServiceTest.java
@@ -13,12 +13,14 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class CulturemediaServiceTest {
     private VideoRepository videoRepository;
     private ViewsRepository viewsRepository;
     private CulturemediaService culturemediaService;
+      private List<Video> videos;
 
     @BeforeEach
     void setUp() {
@@ -49,5 +51,34 @@ class CulturemediaServiceTest {
 
         assertThrows(VideoNotFoundException.class, executable);
     } 
-    
+
+    @Test
+    void find_by_title_video_exception() {
+        assertThrows(VideoNotFoundException.class, () -> culturemediaService.findByTitle("title"));
+    }
+
+    @Test
+    void find_by_duration_video_exception() {
+        assertThrows(VideoNotFoundException.class, () -> culturemediaService.findByDuration(0.0, 5.0));
+    }
+
+    @Test
+    void find_by_title_video() throws VideoNotFoundException {
+        String parameter = "Video";
+        List<Video> expected = videos.stream().filter(p -> p.title().contains(parameter)).toList();
+        doReturn(expected).when(videoRepository).find(parameter);
+
+        List<Video> result = culturemediaService.findByTitle(parameter);
+        assertTrue(result.containsAll(expected));
+    }
+
+    @Test
+    void find_by_duration_video() throws VideoNotFoundException {
+        List<Video> expected = videos.stream().filter(p -> p.duration() <= 5.0 && p.duration() >= 3.0).toList();
+        doReturn(expected).when(videoRepository).find(3.0, 5.0);
+
+        List<Video> result = culturemediaService.findByDuration(3.0, 5.0);
+        assertTrue(result.containsAll(expected));
+    }
+
 }
