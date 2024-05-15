79613913c450843ea0aa711b0337cdcd67770801
yiyilopez <146986398+yiyilopez@users.noreply.github.com>
2024-05-15 18:23:33 -0500

    new

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -2,11 +2,10 @@ package culturemedia.repository;
 
 import java.util.List;
 
-import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 
 public interface VideoRepository {
-    List<Video> findAll() throws VideoNotFoundException;
+    List<Video> findAll();
     Video save(Video save);
     List<Video> find(String title);
     List<Video> find(Double fromDuration, Double toDuration);
